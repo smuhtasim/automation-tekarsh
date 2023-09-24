9de6332a6ff3a3821225b75108a9a3ead7e9a23a
smuhtasim <srijonmuhtasim@gmail.com>
2023-09-24 23:00:25 +0600

    final done

--- cypress/pages/cartPage.js
+++ cypress/pages/cartPage.js
@@ -1,33 +1,37 @@
 class CartPage {
+  constructor() {
+    this.PRICE = 0;
+    this.QUANTITY = 0
+  }
+
   visit() {
     cy.visit("/view_cart");
+  }
+
+  calculateTotal(price, quantity) {
+    return price * quantity;
   }
 
   verifyData() {
     cy.get("tr")
       .eq(1)
       .within(() => {
-        debugger;
-        const price = parseInt(
-          cy.get(".cart_price").within(() => {
-            cy.get("p")
-              .invoke("text")
-              .then((text) => {
-                text.replace(/[^0-9.]/g, "");
-              });
-          })
-        );
-        cy.log("this is the price", price);
-        const quantity = parseInt(
-          cy.get(".cart_quantity").within(() => {
-            cy.get("button").invoke("text");
-          })
-        );
-        cy.log("**" + price + "##" + quantity);
-        const total = price * quantity;
-        cy.log("total", total);
-        cy.get(".cart_total_price").should("have.text", `Rs. ${total}`);
+        cy.get(".cart_price p")
+          .invoke("text")
+          .then((text) => {
+            this.PRICE = parseInt(text.match(/-?\d+/)[0], 10);
+          });
+
+        cy.get(".cart_quantity button")
+          .invoke("text")
+          .then((text) => {
+            this.QUANTITY = parseInt(text.match(/-?\d+/)[0], 10);
+          });
       });
+
+    const total = this.PRICE * this.QUANTITY;
+
+    cy.get(".cart_total_price").should("have.text", `Rs. ${total}`);
   }
 
   selectProduct() {
@@ -43,7 +47,7 @@ class CartPage {
   }
 
   clickCheckoutButton() {
-    this.getCheckoutButton().click()
+    this.getCheckoutButton().click();
   }
 }
 
